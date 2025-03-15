eecab1a1c0d71263cbd15edeff766b51f963192a
Liya-dotcom <simethebe@gmail.com>
2025-03-15 22:56:58 +0200

    Ininitial commit

--- answers.sql
+++ answers.sql
@@ -1,9 +1,12 @@
 # Coonecting to the database Server
 con = connect.sql.connect(
-    host="localhoet",
+    host="localhost",
     user="root",
     password="12345",
 )
+
+# Creating a cursor object 
+cursor = con.cursor()
 
 # Creating database
 database_name ="salesDB"
@@ -16,6 +19,16 @@ cursor.close()
 con.close()
 
 
+# Reconneting to the database
+con = connect.sql.connect(
+    host="localhost",
+    user="root",
+    password="12345"
+)
+
+# Creating a cursor object
+cursor  = conn.cursor()
+
 # Creating database demo
 database_name ="demoDB",
 cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
@@ -25,3 +38,7 @@ print(f"Database '{database_name}' created successfully")
 # Dropping database demoDB
 cursor = con.execute(f"DROP DATABASE IF EXISTS demoDB")
 print("Database 'demoDB' dropped successfully")
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
